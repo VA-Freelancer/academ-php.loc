71b432c7d6d50924e0c09cc527d0c6ca233cccd8
Vikulov Andrey <vikulov.prog@gmail.com>
2021-11-18 00:57:08 +0400

    Initial commit

---
+++ portfolio/data.php
@@ -0,0 +1,35 @@
+<?
+
+return [
+    'avatar' => 'https://sun9-59.userapi.com/impg/Dabpaa7wCRhNcbWBq8obE902ZJ8xKrNwpB5wzQ/wCLUDDtvuyc.jpg?size=1080x1080&quality=95&sign=0d4b40b523e375b22c11a5e043e67df9&type=album',
+    'about' => [
+        'name' => 'Иван Иванов',
+        'post' => 'Frontend Developer',
+        'email' => 'vproger@vk.com',
+        'phone' => '+79120100525',
+        'site' => 'https://vk.com/vproger',
+    ],
+    'education' =>[
+        [
+            'faculty' => 'Нет',
+            'university' => 'Нет',
+            'yearStart' => 'Нет',
+            'yearEnd' => 'Нет'
+        ],
+        [
+            'faculty' => 'Нет',
+            'university' => 'Нет',
+            'yearStart' => 'Нет',
+            'yearEnd' => 'Нет'
+        ]
+    ],
+    'language' =>[
+        'Russian' => 'Native',
+        'English' => 'Basic',
+        'Spanish' => 'Basic',
+    ],
+    'interests' => [
+        'IT Knowledge', 'Web Design'
+    ],
+
+];
